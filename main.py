d84d47ca1c901c6a730aedece83b9fe1331955b2
Md Hasibul Hasan <mdhasibulhasan.iubat@gmail.com>
2021-02-16 03:24:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,61 +7,46 @@ nl = [['Shuvo', 24, 'SanFrancisco', 'United States'],
             ['Jamil', 22, 'Rome', 'Italy'], 
             ['Imtiaj', 21, 'New York', 'United States'], 
             ['Rafi', 20, 'Las Vegas', 'United States'], 
-            ['Hasan', 19, 'Berlin', 'Germany']] 
-  
-# Create a DataFrame object 
+            ['Hasan', 19, 'Berlin', 'Germany']]   
+# create a DataFrame object 
 df = pd.DataFrame(nl, 
                   columns=['Name', 'Age', 'City', 'Country'], 
-                  ) 
-  
-# Creating 2 lists 'marks' and 'gender' 
+                  )  
+# creating 2 lists 'marks' and 'gender' 
 marks = [99.5,98.9,97.2,96.2,95.5,94.5] 
-gender = ['M','M','M','M','M','M'] 
-  
+gender = ['M','M','M','M','M','M']   
 # adding lists as new column to dataframe df 
 df['Marks'] = marks 
 df['Gender'] = gender 
-
 # Creating 1 lists 'status'
 status=['Married','Married','Single','Singe','Single','Single']
-
 # adding lists as new column to dataframe df 
-df['Status'] = status
-  
-#Creating CSV file
+df['Status'] = status 
+# creating CSV file
 df.to_csv('die.csv') 
-
-# Displaying the Data frame 
+# displaying the Data frame 
 df
 
 
-#Updating CSV File
-
-df = pd.read_csv("die.csv") 
-  
+# updating CSV File
+df = pd.read_csv("die.csv")  
 # updating the column value/data 
-df.loc[1, 'Marks'] = '70.9'
-  
-# Displaying the Data frame 
+df.loc[1, 'Marks'] = '70.9' 
+# displaying the Data frame 
 df
 
 
-#Delete Data From CSV File Row Wise
-df = pd.read_csv("die.csv", index_col ="Name" ) 
-
-  
+# delete Data From CSV File Row Wise
+df = pd.read_csv("die.csv", index_col ="Name" )   
 # dropping passed values 
 df.drop(["Hasan"], inplace = True) 
-
-# display 
+# displaying the Data frame 
 df
 
-#Delete Data From CSV File Column Wise  
+# delete Data From CSV File Column Wise  
 df = pd.read_csv("die.csv", index_col ="Name" ) 
-
 # dropping passed columns 
-df.drop(["Status"], axis = 1, inplace = True) 
-  
-# display 
+df.drop(["Status"], axis = 1, inplace = True)  
+# displaying the Data frame 
 df
 
